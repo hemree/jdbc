0c3a98e10a89ab4b19623c45a40ba843c5461246
Huseyin Emre Bolum <119007948+hemree@users.noreply.github.com>
2023-05-02 23:19:53 +0300

    first commit

---
+++ src/main/java/Execute01.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Execute01 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Techpro","postgres","1543");
+        Statement st = con.createStatement();
+        System.out.println("Connection Success");
+
+    }
+}
